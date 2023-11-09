7ac0e79efb79dba3e0b89f99c7213e036de213ec
Fedasevich <yurafedasevka@gmail.com>
2023-11-09 22:39:09 +0100

    revert: accidental auto merge
    This reverts commit fdfac6e139dd6f509001534ba2d5a2753db64c90.

--- src/locales/langs/en.ts
+++ src/locales/langs/en.ts
@@ -2,14 +2,6 @@ const en = {
   app_title: 'App title',
   app: `this application`,
   example: 'text for demo purposes',
-  signUpFourthForm: {
-    passwordInvalid: 'Password needs to be at least 8 characters',
-    passwordsNotMatching: 'Passwords field and Confirm password field must match',
-    passwordRequired: 'Password is required',
-    confirmPasswordRequired: 'Confirm password is required',
-    placeholderPassword: 'Password',
-    placeholderConfirmPassword: 'Confirm Password',
-  },
 };
 
 export default en;

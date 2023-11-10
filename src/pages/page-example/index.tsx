f7f550ba13368fe584cca8d042cac9b5f463ef74
Nadiia Tsytsylina <miniova95@gmail.com>
2023-11-10 19:00:41 +0200

    fix: update page-example
    sorry, by accident pushed this changes

--- src/pages/page-example/index.tsx
+++ src/pages/page-example/index.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import Head from 'next/head';
 import ExampleForm from '../../components/example-form';
-import SignUpSecond from 'src/components/sign-up-second/SignUpSecond';
 
 export default function ExamplePage(): JSX.Element {
-  const caregiver = 'caregiver';
   return (
     <>
       <Head>
         <title> Example page title</title>
       </Head>
-      <SignUpSecond role={caregiver} />
+      <ExampleForm text="example" />
     </>
   );
 }

63cb1a595422d8b95710480373bac5a6ac6893b3
Nadiia Tsytsylina <miniova95@gmail.com>
2023-11-14 13:12:11 +0200

    feat: Need help modal
    create NeedHelpModal component;
    add styles;
    update Header and styles;
    add Cross icon;
    add colors, that I needed, to theme;

--- src/locales/langs/en.ts
+++ src/locales/langs/en.ts
@@ -2,11 +2,18 @@ const en = {
   app_title: 'App title',
   app: `this application`,
   example: 'text for demo purposes',
-  SignUp: "Sign Up",
-  seeker: 'Find the perfect caregiver for you or your loved one to get reliable and personalized care',
-  caregiver: 'Connect with clients seeking your expertise and provide compassionate care for those in need',
-  BySigningUp: "By signing up you agree to the ",
-  terms_conditions: "Terms and Conditions",
+  SignUp: 'Sign Up',
+  seeker:
+    'Find the perfect caregiver for you or your loved one to get reliable and personalized care',
+  caregiver:
+    'Connect with clients seeking your expertise and provide compassionate care for those in need',
+  BySigningUp: 'By signing up you agree to the ',
+  terms_conditions: 'Terms and Conditions',
+  needHelpModal: {
+    title: 'Need Help',
+    text: 'Reach out to us via email and we`ll get back to you as soon as possible:',
+    email: 'support@afyanexcare.com',
+  },
 };
 
 export default en;

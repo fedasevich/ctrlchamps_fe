f1d2804a3ff837cf76084648a26d2865f2fffa1f
Nadiia Tsytsylina <miniova95@gmail.com>
2023-11-16 16:33:52 +0200

    Update TermsModal.tsx

--- src/components/terms-modal/TermsModal.tsx
+++ src/components/terms-modal/TermsModal.tsx
@@ -1,29 +1,44 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useLocales } from 'src/locales';
-import TermsModal from 'src/components/terms-modal/TermsModal';
-import { Footer, Text, TermsButton } from './styles';
+import Cross from 'src/assets/icons/Cross';
+import {
+  BackDrop,
+  Modal,
+  ModalHeader,
+  CloseButton,
+  HeaderTitle,
+  TextContainer,
+  HeadText,
+  TextList,
+} from './styles';
 
-export default function SignUpFooter(): JSX.Element {
+interface IProps {
+  onClose: () => void;
+}
+
+export default function TermsModal({ onClose }: IProps): JSX.Element {
   const { translate } = useLocales();
-  const [showModal, setShowModal] = useState<boolean>(false);
-
-  const handleClick = (): void => {
-    setShowModal(true);
-  };
-
-  const onClose = (): void => {
-    setShowModal(false);
-  };
 
   return (
-    <>
-      <Footer>
-        <Text>{translate('footer.termsText')}</Text>
-        <TermsButton type="button" onClick={handleClick}>
-          {translate('footer.termsButton')}
-        </TermsButton>
-      </Footer>
-      {showModal && <TermsModal onClose={onClose} />}
-    </>
+    <BackDrop>
+      <Modal>
+        <ModalHeader>
+          <CloseButton type="button" onClick={onClose}>
+            <Cross />
+          </CloseButton>
+          <HeaderTitle>{translate('termsModal.title')}</HeaderTitle>
+        </ModalHeader>
+        <TextContainer>
+          <HeadText>{translate('termsModal.headText')}</HeadText>
+          <TextList>
+            <li>{translate('termsModal.firstItem')}</li>
+            <li>{translate('termsModal.secondItem')}</li>
+            <li>{translate('termsModal.thirdItem')}</li>
+            <li>{translate('termsModal.fourthItem')}</li>
+            <li>{translate('termsModal.fifthItem')}</li>
+          </TextList>
+        </TextContainer>
+      </Modal>
+    </BackDrop>
   );
 }

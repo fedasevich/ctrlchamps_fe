75afee5715d33997a7b566b007692861b58db638
ProggerOleg <olegtar25@gmail.com>
2023-11-16 16:52:58 +0200

    chore: self check, fixed imports order

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -2,18 +2,18 @@ import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 
+import { RootState } from 'src/redux/store';
+import { useSignUpMutation } from 'src/redux/api/userAPI';
+
 import Step1Form from 'src/components/sign-up/sign-up-first/SignUpForm1';
 import SignUpThirdForm from 'src/components/sign-up-third';
 import SignUpHeader from 'src/components/reusable/header';
-import { RootState } from 'src/redux/store';
-import { useSignUpMutation } from 'src/redux/api/userAPI';
 
 const SignUp = (): JSX.Element => {
   const { t } = useTranslation();
 
   const [step, setStep] = useState<number>(1);
-  const selectedOption = useSelector((state: RootState) => state.auth.selectedOption);
-  const addressData = useSelector((state: RootState) => state.address.addressData);
+  const selectedOption :string = useSelector((state: RootState) => state.auth.selectedOption);
   
   const [signUp] = useSignUpMutation();
 

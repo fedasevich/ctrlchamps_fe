8ba81dbdb758f345848ae71894af7ee8f8848532
Fedasevich <yurafedasevka@gmail.com>
2023-11-17 09:53:39 +0100

    fix: remove magic number

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,25 +1,27 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 
-import { RootState } from 'src/redux/store';
 import { useSignUpMutation } from 'src/redux/api/userAPI';
+import { RootState } from 'src/redux/store';
 
-import Step1Form from 'src/components/sign-up/sign-up-first/SignUpForm1';
-import SignUpThirdForm from 'src/components/sign-up-third';
 import SignUpHeader from 'src/components/reusable/header';
+import SignUpThirdForm from 'src/components/sign-up-third/SignUpThirdForm';
+import Step1Form from 'src/components/sign-up/sign-up-first/SignUpForm1';
+
+const FIRST_STEP = 1;
 
 const SignUp = (): JSX.Element => {
   const { t } = useTranslation();
 
-  const [step, setStep] = useState<number>(1);
-  const selectedOption :string = useSelector((state: RootState) => state.auth.selectedOption);
-  
+  const [step, setStep] = useState<number>(FIRST_STEP);
+  const selectedOption: string = useSelector((state: RootState) => state.auth.selectedOption);
+
   const [signUp] = useSignUpMutation();
 
   const handleSignUp = async (): Promise<void> => {
     const userInfo = {
-      role: selectedOption
+      role: selectedOption,
     };
     try {
       await signUp({ userInfo });
@@ -31,7 +33,7 @@ const SignUp = (): JSX.Element => {
   const handleNextStep = (): void => {
     handleSignUp()
       .then(() => {
-        setStep(prevStep => prevStep + 1);
+        setStep((prevStep) => prevStep + 1);
       })
       .catch((error) => {
         throw new Error(error);

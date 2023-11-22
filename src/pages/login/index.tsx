3f0b67b185bed0399c3a755fd6bb30c2a8f47e2f
ProggerOleg <olegtar25@gmail.com>
2023-11-22 12:52:18 +0200

    fix: returned accidentally removed code, changed the header to a new one in the components

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { useLocales } from 'src/locales';
 import LoginWrapper from 'src/components/login-form/LoginWrapper';
-import SignUpHeader from 'src/components/reusable/header';
+import FlowHeader from 'src/components/reusable/header/FlowHeader';
 import LoginForm from 'src/components/login-form/LoginForm';
 import SignUpFooter from 'src/components/reusable/footer';
 
 export default function Login(): JSX.Element {
   const { translate } = useLocales();
+  const { back: pushBack } = useRouter();
+
   return (
     <>
       <Head>
         <title>{translate('loginForm.title')}</title>
       </Head>
-      <SignUpHeader text={translate('loginForm.title')} />
+      <FlowHeader text={translate('loginForm.title')} callback={pushBack} iconType="back" />
       <LoginWrapper>
         <>
           <LoginForm />

cd9b50717c9bcb8f8f62e00f41ee676feaa769e8
Nadiia Tsytsylina <miniova95@gmail.com>
2023-11-30 15:53:52 +0200

    fix: update BE request

--- src/components/complete-profile-third/hooks.ts
+++ src/components/complete-profile-third/hooks.ts
@@ -1,6 +1,3 @@
-import jwt_decode from 'jwt-decode';
-
-import { useTypedSelector } from 'src/redux/store';
 import { useUpdateProfileMutation } from 'src/redux/api/profileCompleteApi';
 import { SPLIT_SEPARATOR, JOIN_SEPARATOR } from 'src/components/complete-profile-third/constants';
 import { ReturnType } from 'src/components/complete-profile-third/types';
@@ -10,8 +7,8 @@ interface Data {
 }
 
 export function useCompleteProfileThird(onNext: () => void): ReturnType {
-  const token = useTypedSelector((state) => state.token.token);
   const [updateServices] = useUpdateProfileMutation();
+
   const onUpdateServices = async (data: Data): Promise<void> => {
     const resultArray: string[] = [];
     const trueKeys = Object.keys(data).filter((elem) => data[elem] === true);
@@ -19,11 +16,9 @@ export function useCompleteProfileThird(onNext: () => void): ReturnType {
       const newElement = element.split(SPLIT_SEPARATOR).join(JOIN_SEPARATOR);
       resultArray.push(newElement);
     });
-    const decoded: { id: string } = jwt_decode(token);
+
     try {
       await updateServices({
-        userId: decoded.id,
-        token,
         updateProfileDto: { services: resultArray },
       })
         .unwrap()

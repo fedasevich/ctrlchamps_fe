ac12a6fe1be92848ff449dd7474809f9195b456f
Fedasevich <yurafedasevka@gmail.com>
2023-12-01 15:12:55 +0100

    feat: add global constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,11 +4,18 @@ export const EMAIL_ERROR = 'email';
 export const PHONE_ERROR = 'phone';
 export const USER_MIN_AGE = 18;
 export const USER_DATE_BIRTH_FORMAT = 'dd.MM.yyyy';
+export const MAX_CHARACTERS_LENGTH = 100;
+export const DATE_FORMAT = 'dd/MM/yyyy';
+export const BACKEND_DATE_FORMAT = 'yyyy-MM-dd';
+export const URL_PATTERN = /^https:\/\/.+$/;
 
 export const USER_ROLE = {
   caregiver: 'caregiver',
   seeker: 'seeker',
 };
+
+export const FIRST_STEP_INDEX = 0;
+export const SECOND_STEP_INDEX = 1;
 
 export const weekDays = [
   { abbr: 'M', value: 'Monday' },

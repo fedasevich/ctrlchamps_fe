4c55e857786e9b2463293b3d52c6c0e6b290741a
Fedasevich <yurafedasevka@gmail.com>
2023-12-01 19:02:40 +0100

    fix: resolve merge conflicts

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -9,10 +9,7 @@ import { addressReducer } from 'src/redux/slices/addressSlice';
 import { availableDaysReducer } from 'src/redux/slices/availableDaysSlice';
 import { personalDetailsReducer } from 'src/redux/slices/personalDetailsSlice';
 import { rateReducer } from 'src/redux/slices/rateSlice';
-import { appointmentReducer } from './slices/appointmentSlice';
-import { servicesReducer } from './slices/servicesSlice';
 import { healthQuestionnaireReducer } from './slices/healthQuestionnaireSlice';
-
 
 const rootReducer = combineReducers({
   role: roleReducer,

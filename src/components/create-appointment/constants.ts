a3897ff5a207580bd829d740ba5c00129dc10a9c
Lilly <nuralievalilly17@gmail.com>
2023-12-02 18:43:25 +0200

    fix: use date-fns library

--- src/components/create-appointment/constants.ts
+++ src/components/create-appointment/constants.ts
@@ -3,13 +3,8 @@ import { generateTimeWithInterval } from 'src/utils/generateTime';
 const MIN_APPOINTMENT_NAME_LENGTH = 5;
 const MAX_APPOINTMENT_NAME_LENGTH = 50;
 const MINUTES_INTERVAL = 15;
-const ONE_DAY_PERIOD = 24 * 60 * 60 * 1000;
+const ONE_DAY = 1;
 
 const selectTimeOptions = generateTimeWithInterval('00:00 AM', '23:45 PM', MINUTES_INTERVAL);
 
-export {
-  MIN_APPOINTMENT_NAME_LENGTH,
-  MAX_APPOINTMENT_NAME_LENGTH,
-  ONE_DAY_PERIOD,
-  selectTimeOptions,
-};
+export { MIN_APPOINTMENT_NAME_LENGTH, MAX_APPOINTMENT_NAME_LENGTH, ONE_DAY, selectTimeOptions };

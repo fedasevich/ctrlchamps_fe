f848e817228188b1d26d9442c1a378122c82fd5e
ProggerOleg <olegtar25@gmail.com>
2023-12-03 15:14:18 +0200

    feat: made health-questionnaire choices be saved properly in slice, displayed right, etc

--- src/components/health-questionnaire/index.tsx
+++ src/components/health-questionnaire/index.tsx
@@ -3,6 +3,11 @@ import Step1 from 'src/components/health-questionnaire/steps/Step1';
 import Step2 from 'src/components/health-questionnaire/steps/Step2';
 import Step3 from 'src/components/health-questionnaire/steps/Step3';
 import { QuestionnaireContainer, Background } from 'src/components/health-questionnaire/styles';
+import {
+  useGetActivityQuery,
+  useGetCapabilityQuery,
+  useGetDiagnosisQuery,
+} from 'src/redux/api/healthQuestionnaireApi';
 
 const STEPS = {
   STEP_1: 'STEP_1',
@@ -16,6 +21,9 @@ type Props = {
 
 const HealthQuestionnaire = ({ onNext }: Props): JSX.Element => {
   const [currentStep, setCurrentStep] = useState(STEPS.STEP_1);
+  const { data: activities } = useGetActivityQuery();
+  const { data: capabilities } = useGetCapabilityQuery();
+  const { data: diagnoses } = useGetDiagnosisQuery();
 
   const handleNext = (): void => {
     setCurrentStep(currentStep === STEPS.STEP_1 ? STEPS.STEP_2 : STEPS.STEP_3);
@@ -31,15 +39,29 @@ const HealthQuestionnaire = ({ onNext }: Props): JSX.Element => {
 
   return (
     <Background>
-      <QuestionnaireContainer>
-        {currentStep === STEPS.STEP_1 && <Step1 onNext={handleNext} stepKey={STEPS.STEP_1} />}
-        {currentStep === STEPS.STEP_2 && (
-          <Step2 onNext={handleNext} onBack={handleBack} stepKey={STEPS.STEP_2} />
-        )}
-        {currentStep === STEPS.STEP_3 && (
-          <Step3 onNext={handleSubmit} onBack={handleBack} stepKey={STEPS.STEP_3} />
-        )}
-      </QuestionnaireContainer>
+      {diagnoses && activities && capabilities ? (
+        <QuestionnaireContainer>
+          {currentStep === STEPS.STEP_1 && (
+            <Step1 onNext={handleNext} stepKey={STEPS.STEP_1} diagnoses={diagnoses} />
+          )}
+          {currentStep === STEPS.STEP_2 && (
+            <Step2
+              onNext={handleNext}
+              onBack={handleBack}
+              stepKey={STEPS.STEP_2}
+              activities={activities}
+            />
+          )}
+          {currentStep === STEPS.STEP_3 && (
+            <Step3
+              onNext={handleSubmit}
+              onBack={handleBack}
+              stepKey={STEPS.STEP_3}
+              capabilities={capabilities}
+            />
+          )}
+        </QuestionnaireContainer>
+      ) : null}
     </Background>
   );
 };

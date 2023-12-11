e32677b5cc008b11f95f3c351c39bf3ebeb7b14a
Fedasevich <yurafedasevka@gmail.com>
2023-12-11 18:25:03 +0100

    fix: remove caregiver list from page

--- src/pages/create-appointment/index.tsx
+++ src/pages/create-appointment/index.tsx
@@ -1,18 +1,18 @@
 import Head from 'next/head';
-import CreateAppointmentFourth from 'src/components/create-appointment-fourth/CreateAppointmentFourth';
+import AppointmentScheduling from 'src/components/create-appointment/AppointmentScheduling';
+import AppointmentType from 'src/components/create-appointment/AppointmentType';
 import { useLocales } from 'src/locales';
 import { useTypedSelector } from 'src/redux/store';
 
 export default function CreateAppointmentPage(): JSX.Element {
   const { translate } = useLocales();
   const { appointmentType } = useTypedSelector((state) => state.appointment);
-
   return (
     <>
       <Head>
         <title>{translate('create_appointment.create')}</title>
       </Head>
-      <CreateAppointmentFourth onNext={() => undefined} />
+      {appointmentType === null ? <AppointmentType /> : <AppointmentScheduling />}
     </>
   );
 }

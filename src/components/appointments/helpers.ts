b4a4b7f6a92f1f1abd0b59228889881d42d3fd66
Tsaturian Volodymyr <vova.zczc@gmail.com>
2023-12-13 00:02:01 +0200

    fix: create const for mock user avatar url

--- src/components/appointments/helpers.ts
+++ src/components/appointments/helpers.ts
@@ -1,11 +1,11 @@
-import { string } from 'yup';
 import { format, parseISO } from 'date-fns';
-import { DRAWER_DATE_FORMAT } from './constants';
+import { DRAWER_DATE_FORMAT, MOCK_USER_AVATAR_URL } from './constants';
 
 export const getMockCaregiverAvatar = (size: number): string =>
-  `https://picsum.photos/${size}/${size}`;
+  `${MOCK_USER_AVATAR_URL}/${size}/${size}`;
 
-export const getFormattedDate = (date: string): string => format(parseISO(date), DRAWER_DATE_FORMAT);
+export const getFormattedDate = (date: string): string =>
+  format(parseISO(date), DRAWER_DATE_FORMAT);
 
 export const mockedAppointments = [
   {

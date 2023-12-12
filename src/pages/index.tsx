8dacbd7268657159ec08a94ee6fdd9f04a872367
Lilly <nuralievalilly17@gmail.com>
2023-12-12 20:52:51 +0200

    fix import order

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 // next
 import Head from 'next/head';
+import { useState } from 'react';
 import Appointments from 'src/components/appointments/Appointments';
 import BookAppointment from 'src/components/book-appointment/BookAppointment';
 import MainHeader from 'src/components/reusable/header/MainHeader';
 import { useLocales } from 'src/locales';
 import { mockedAppointments } from 'src/components/appointments/helpers';
 import CaregiverSchedule from 'src/components/caregiver-schedule/CaregiverSchedule';
-import { useState } from 'react';
 import { ActiveTab } from 'src/components/reusable/header/types';
 import { TabType } from 'src/components/reusable/header/enums';
 

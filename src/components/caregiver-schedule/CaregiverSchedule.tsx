4dc602c80899330e32a190392160ea0cdcca3fbb
Lilly <nuralievalilly17@gmail.com>
2023-12-13 14:27:50 +0200

    change logic to show only one day + remove unused imports

--- src/components/caregiver-schedule/CaregiverSchedule.tsx
+++ src/components/caregiver-schedule/CaregiverSchedule.tsx
@@ -1,12 +1,11 @@
 import { CalendarPicker } from '@mui/x-date-pickers';
-import { addDays } from 'date-fns';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import CreateAppointmentIcon from 'src/assets/icons/CreateAppointmentIcon';
 import AppointmentDrawer from 'src/components/appointments/appointment-drawer/AppointmentDrawer';
+import { CURRENT_DAY } from 'src/constants';
 import { useLocales } from 'src/locales';
-import { CURRENT_DAY, ONE_DAY } from 'src/constants';
 import CaregiverAppointment from './CaregiverAppointment';
-import { mocksChosenDay, mokcsNextDay } from './mocks';
+import { mocksChosenDay } from './mocks';
 import {
   AppointmentsContainer,
   Background,
@@ -29,22 +28,17 @@ export default function CaregiverSchedule({
 
   const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
   const [chosenDay, setChosenDay] = useState<Date>(CURRENT_DAY);
-  const [nextDay, setNextDay] = useState<Date>(addDays(chosenDay, ONE_DAY));
   const [chosenDayAppointments, setChosenDayAppointments] = useState<CaregiverAppointmentI[]>([]);
-  const [nextDayAppointments, setNextDayAppointments] = useState<CaregiverAppointmentI[]>([]);
 
   const chooseDay = (date: Date | null): void => {
     if (date) {
       setChosenDay(date);
-      setNextDay(addDays(date, ONE_DAY));
       setChosenDayAppointments(mocksChosenDay);
-      setNextDayAppointments(mokcsNextDay);
     }
   };
 
   const chooseCurrentDate = (): void => {
     setChosenDay(CURRENT_DAY);
-    setNextDay(addDays(CURRENT_DAY, ONE_DAY));
   };
 
   const openDrawer = (): void => setIsDrawerOpen(true);
@@ -88,13 +82,6 @@ export default function CaregiverSchedule({
               appointmentDay={chosenDay}
               openDrawer={openDrawer}
             />
-            {nextDayAppointments.length > 0 && (
-              <CaregiverAppointment
-                appointmentDays={nextDayAppointments}
-                appointmentDay={nextDay}
-                openDrawer={openDrawer}
-              />
-            )}
           </AppointmentsContainer>
         )}
       </Container>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
-// next
 import Head from 'next/head';
 import { useState } from 'react';
-import Appointments from 'src/components/appointments/Appointments';
-import BookAppointment from 'src/components/book-appointment/BookAppointment';
-import MainHeader from 'src/components/reusable/header/MainHeader';
-import { useLocales } from 'src/locales';
-import { mockedAppointments } from 'src/components/appointments/helpers';
+
 import CaregiverSchedule from 'src/components/caregiver-schedule/CaregiverSchedule';
-import { ActiveTab } from 'src/components/reusable/header/types';
+import MainHeader from 'src/components/reusable/header/MainHeader';
 import { TabType } from 'src/components/reusable/header/enums';
+import { ActiveTab } from 'src/components/reusable/header/types';
+import { useLocales } from 'src/locales';
 
 export default function HomePage(): JSX.Element {
   const { translate } = useLocales();

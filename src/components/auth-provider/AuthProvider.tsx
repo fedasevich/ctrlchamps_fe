cbde6edef3ef23066d4b4c5f204ab26510c0a2c2
Fedasevich <yurafedasevka@gmail.com>
2023-12-15 11:19:51 +0100

    feat: add auth provider

---
+++ src/components/auth-provider/AuthProvider.tsx
@@ -0,0 +1,32 @@
+import jwt_decode from 'jwt-decode';
+import { useEffect, useState } from 'react';
+import { User, setUser } from 'src/redux/slices/userSlice';
+import { useAppDispatch, useTypedSelector } from 'src/redux/store';
+
+interface AuthProviderProps {
+  children: React.ReactNode;
+}
+
+export function AuthProvider({ children }: AuthProviderProps): JSX.Element | null {
+  const dispatch = useAppDispatch();
+
+  const [loading, setLoading] = useState(true);
+
+  const token = useTypedSelector((state) => state.token.token);
+
+  useEffect(() => {
+    if (!token) {
+      setLoading(false);
+      return;
+    }
+    const user = jwt_decode<User>(token);
+    dispatch(setUser(user));
+    setLoading(false);
+  }, [dispatch, token]);
+
+  if (loading) {
+    return null;
+  }
+
+  return <>{children}</>;
+}

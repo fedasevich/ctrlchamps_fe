22df1dee2a6b4b3b9803ffef5fe2d93a3394cf0d
Fedasevich <yurafedasevka@gmail.com>
2023-12-15 11:19:31 +0100

    feat: add user slice

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -5,14 +5,15 @@ import { appointmentReducer } from 'src/redux/slices/appointmentSlice';
 import { availableDaysReducer } from 'src/redux/slices/availableDaysSlice';
 import { caregiverReducer } from 'src/redux/slices/caregiverSlice';
 import { certificateReducer } from 'src/redux/slices/certificateSlice';
+import { healthQuestionnaireReducer } from 'src/redux/slices/healthQuestionnaireSlice';
+import { locationReducer } from 'src/redux/slices/locationSlice';
 import { personalDetailsReducer } from 'src/redux/slices/personalDetailsSlice';
 import { rateReducer } from 'src/redux/slices/rateSlice';
 import { roleReducer } from 'src/redux/slices/roleSlice';
+import { servicesReducer } from 'src/redux/slices/servicesSlice';
 import { tokenReducer } from 'src/redux/slices/tokenSlice';
 import { workExperienceReducer } from 'src/redux/slices/workEperienceSlice';
-import { healthQuestionnaireReducer } from 'src/redux/slices/healthQuestionnaireSlice';
-import { locationReducer } from 'src/redux/slices/locationSlice';
-import { servicesReducer } from 'src/redux/slices/servicesSlice';
+import { userReducer } from './slices/userSlice';
 
 const rootReducer = combineReducers({
   role: roleReducer,
@@ -28,6 +29,7 @@ const rootReducer = combineReducers({
   hourlyRate: rateReducer,
   appointment: appointmentReducer,
   location: locationReducer,
+  user: userReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;

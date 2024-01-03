4b18e4811b63db7d3bfddb2f16d64d8f3cba044c
Fedasevich <yurafedasevka@gmail.com>
2024-01-03 12:43:38 +0100

    fix: move styles from page

--- src/pages/faq/index.tsx
+++ src/pages/faq/index.tsx
@@ -1,11 +1,11 @@
 import Head from 'next/head';
+
 import { Background, HeadContainer, Title } from 'src/components/appointments/styles';
 import FaqAccordion from 'src/components/faq-accordion/FaqAccordion';
+import { StyledContainer } from 'src/components/faq-accordion/styles';
 import { PrivateRoute } from 'src/components/private-route/PrivateRoute';
-
 import MainHeader from 'src/components/reusable/header/MainHeader';
 import { useLocales } from 'src/locales';
-import { StyledContainer } from './styles';
 
 export default function FaqPage(): JSX.Element | null {
   const { translate } = useLocales();

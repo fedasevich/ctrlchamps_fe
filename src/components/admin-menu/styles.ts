40ac50d26c67f484cd4dc90d235f97e59567972d
Tsaturian Volodymyr <vova.zczc@gmail.com>
2024-01-08 18:54:50 +0200

    fix: correct menu border

--- src/components/admin-menu/styles.ts
+++ src/components/admin-menu/styles.ts
@@ -5,7 +5,7 @@ import { TYPOGRAPHY } from 'src/theme/fonts';
 
 export const MenuList = styled(MuiMenuList)`
   width: 230px;
-  height: calc(100vh - 20px);
+  min-height: 100vh;
   padding-top: 60px;
   border-right: 1px solid;
 `;

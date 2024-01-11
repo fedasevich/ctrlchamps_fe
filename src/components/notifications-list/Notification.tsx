3b20aa164e8b2f09a6d5eb21986aa0875c72e5db
Lilly <nuralievalilly17@gmail.com>
2024-01-11 21:34:22 +0200

    add caregiver drawer

--- src/components/notifications-list/Notification.tsx
+++ src/components/notifications-list/Notification.tsx
@@ -1,5 +1,5 @@
 import { IconButton } from '@mui/material';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import RightAction from 'src/assets/icons/RightAction';
 import { useLocales } from 'src/locales';
@@ -8,6 +8,8 @@ import { useTypedSelector } from 'src/redux/store';
 import { NotificationStatus } from 'src/redux/api/notificationsApi';
 import AppointmentDrawer from 'src/components/appointments/appointment-drawer/AppointmentDrawer';
 
+import CaregiverDrawer from 'src/components/reusable/drawer/caregiver-drawer/CaregiverDrawer';
+import { useGetAppointmentQuery } from 'src/redux/api/appointmentApi';
 import { NOTIFICATION_STATUS } from './constants';
 import { BaseText, BoldText, IconBackground, ListItem } from './styles';
 
@@ -20,13 +22,27 @@ type Props = {
 export default function NotificationItem({ status, username, appointmentId }: Props): JSX.Element {
   const { translate } = useLocales();
   const user = useTypedSelector((state) => state.user.user);
+  const appointmentInfo = useGetAppointmentQuery(appointmentId);
 
+  const [caregiverId, setCaregiverId] = useState<string>('');
   const [isDrawerOpen, setIsDrawerOpen] = useState<boolean>(false);
+  const [isCaregiverDrawerOpen, setIsCaregiverDrawerOpen] = useState<boolean>(false);
 
   const { text, icon } = NOTIFICATION_STATUS[status];
 
+  useEffect(() => {
+    if (appointmentInfo.data?.caregiverInfoId && isDrawerOpen) {
+      setCaregiverId(appointmentInfo.data?.caregiverInfoId);
+    }
+  }, [appointmentInfo.data?.caregiverInfoId, isDrawerOpen]);
+
   const handleDrawerOpen = (): void => setIsDrawerOpen(true);
   const handleDrawerClose = (): void => setIsDrawerOpen(false);
+
+  const closeCaregiverDrawer = (): void => {
+    setIsDrawerOpen(true);
+    setIsCaregiverDrawerOpen(false);
+  };
 
   return (
     <>
@@ -52,8 +68,18 @@ export default function NotificationItem({ status, username, appointmentId }: Pr
           role={user.role}
           isOpen={isDrawerOpen}
           setIsDrawerOpen={setIsDrawerOpen}
+          setCaregiverId={setCaregiverId}
+          setIsCaregiverDrawerOpen={setIsCaregiverDrawerOpen}
           onClose={handleDrawerClose}
           selectedAppointmentId={appointmentId}
+        />
+      )}
+
+      {isCaregiverDrawerOpen && caregiverId && (
+        <CaregiverDrawer
+          open={isCaregiverDrawerOpen}
+          onClose={closeCaregiverDrawer}
+          caregiverId={caregiverId}
         />
       )}
     </>

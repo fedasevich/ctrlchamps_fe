fcb263ef007816f88f99c5dc6176b69454ff29b2
ProggerOleg <olegtar25@gmail.com>
2024-01-12 09:31:32 +0200

    fix: fixed comment

--- src/utils/calculateDateDifference.ts
+++ src/utils/calculateDateDifference.ts
@@ -1,6 +1,6 @@
 import { intervalToDuration } from 'date-fns';
 
-// function to calculate end time index based on start time
+// function to calculate duration of one single appointment
 //  @param startTime - "2022-12-22 00:30:00"
 //  @param endTime - "2022-12-22 02:45:00"
 //  @return -  2h 15m`

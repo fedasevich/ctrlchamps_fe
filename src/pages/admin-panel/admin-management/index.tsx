afa32c00d16b08db214ffb247cc34c8dea34e331
Fedasevich <yurafedasevka@gmail.com>
2024-01-19 20:32:38 +0100

    fix: fix all deploy bugs

--- src/pages/admin-panel/admin-management/index.tsx
+++ src/pages/admin-panel/admin-management/index.tsx
@@ -2,10 +2,10 @@ import Head from 'next/head';
 
 import AdminManagement from 'src/components/admin-management/AdminManagement';
 import AdminMenu from 'src/components/admin-menu/AdminMenu';
+import { AdminPageStyledStack } from 'src/components/admin-menu/styles';
 import { PrivateRoute } from 'src/components/private-route/PrivateRoute';
 import { USER_ROLE } from 'src/constants';
 import { useLocales } from 'src/locales';
-import { StyledStack } from '../styles';
 
 export default function AdminManagementPage(): JSX.Element {
   const { translate } = useLocales();
@@ -15,10 +15,10 @@ export default function AdminManagementPage(): JSX.Element {
       <Head>
         <title>{translate('adminManagement.title')}</title>
       </Head>
-      <StyledStack>
+      <AdminPageStyledStack>
         <AdminMenu />
         <AdminManagement />
-      </StyledStack>
+      </AdminPageStyledStack>
     </PrivateRoute>
   );
 }
